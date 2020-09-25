868a779a5fd3e6d746d66ee765073216a666791d
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-09-26 00:30:13 +0530

    Added uc9

--- EmpWageBuilderUC7.java
+++ EmpWageBuilderUC7.java
@@ -1,15 +1,29 @@
 package OopEmpWageBuilder;
 
 public class EmpWageBuilderUC7 {
+
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	
-	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-		int empHrs=0, totalEmpHrs = 0, totalWorkingDays = 0;
-		while(totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+
+	public EmpWageBuilderUC7(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public void computeEmpWage() {
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
-			int empCheck =(int) Math.floor(Math.random()*10)%3;
-			switch(empCheck) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
 			case IS_PART_TIME:
 				empHrs = 4;
 				break;
@@ -20,15 +34,23 @@ public class EmpWageBuilderUC7 {
 				empHrs = 0;
 			}
 			totalEmpHrs += empHrs;
-			System.out.println("Day#" +totalWorkingDays + " Emp Hr: " +empHrs);
+			System.out.println("Day#" + totalWorkingDays + " Emp Hr: " + empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * empRatePerHour;
-		System.out.println("Total Emp Wage for Company: "+ company +" is: "+ totalEmpWage);
-		return totalEmpWage;
+		totalEmpWage = totalEmpHrs * empRatePerHour;
 	}
+
+	@Override
+	public String toString() {
+		return "Total Emp Wage for company : " + company + " is : " + totalEmpWage;
+	}
+
 	public static void main(String[] args) {
-		computeEmpWage("Dmart",20,2,10);
-		computeEmpWage("Reliance",10,4,20);
+		EmpWageBuilderUC7 dmart = new EmpWageBuilderUC7("Dmart", 20, 2, 10);
+		EmpWageBuilderUC7 reliance = new EmpWageBuilderUC7("Reliance", 10, 4, 20);
+		dmart.computeEmpWage();
+		System.out.println(dmart);
+		reliance.computeEmpWage();
+		System.out.println(reliance);
 	}
 
 }
