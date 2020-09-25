28432fa3153f6b80a80b8a84bd110ee926826eff
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-09-25 23:41:04 +0530

    Added UC8

--- EmpWageBuilderUC7.java
+++ EmpWageBuilderUC7.java
@@ -3,13 +3,10 @@ package OopEmpWageBuilder;
 public class EmpWageBuilderUC7 {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR =20;
-	public static final int NUM_OF_WORKING_DAYS = 2;
-	public static final int MAX_HRS_IN_MONTH =10;
 	
-	public static int computeEmpWage() {
+	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 		int empHrs=0, totalEmpHrs = 0, totalWorkingDays = 0;
-		while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while(totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 			int empCheck =(int) Math.floor(Math.random()*10)%3;
 			switch(empCheck) {
@@ -25,12 +22,13 @@ public class EmpWageBuilderUC7 {
 			totalEmpHrs += empHrs;
 			System.out.println("Day#" +totalWorkingDays + " Emp Hr: " +empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Emp Wage: "+ totalEmpWage);
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total Emp Wage for Company: "+ company +" is: "+ totalEmpWage);
 		return totalEmpWage;
 	}
 	public static void main(String[] args) {
-		computeEmpWage();
+		computeEmpWage("Dmart",20,2,10);
+		computeEmpWage("Reliance",10,4,20);
 	}
 
 }
